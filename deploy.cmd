7fa2b119961c5e8d6657399c9101391c3c023787
JohnnyK <spammeherejk@outlook.com>
2016-03-12 15:25:05 -0500

    deploy.cmd popd?

--- deploy.cmd
+++ deploy.cmd
@@ -102,7 +102,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
-  popd
+  :: popd
 )
 
 :: 4. Install bower components
