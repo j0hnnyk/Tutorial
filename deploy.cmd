fd053b804ee8ae29a29506d781177c22d530225f
JohnnyK <spammeherejk@outlook.com>
2016-03-12 15:39:38 -0500

    Final deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -97,7 +97,15 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 :: 2. Select node version
 call :SelectNodeVersion
 
-:: 3. Install bower components
+:: 3. Install npm packages
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install --production
+  IF !ERRORLEVEL! NEQ 0 goto error
+  :: popd
+)
+
+:: 4. Install bower components
 IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call bower install
